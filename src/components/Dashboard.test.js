01b74433083ec45cf942ef4c7efcdf0b56ed7a30
Paul Farino <paul.farino1@gmail.com>
2017-02-09 23:13:16 -0800

    Update tests

--- src/components/Dashboard.test.js
+++ src/components/Dashboard.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Dashboard from './components/Dashboard';
+import Dashboard from './Dashboard';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
